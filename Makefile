2453708444aff830d36929e5414443d3c528a366
MisterMocho <lmcp95@gmail.com>
2026-07-30 17:12:35 +0100

    rag_skeleton

---
+++ Makefile
@@ -0,0 +1,22 @@
+install:
+	uv sync
+
+run:
+	uv run python -m src
+
+debug:
+	uv run python -m pdb -m src
+
+clean:
+	rm -rf __pycache__ .mypy_cache src/__pycache__
+	rm -rf data/processed/*
+
+lint:
+	uv run flake8 src
+	uv run mypy src --warn-return-any --warn-unused-ignores --ignore-missing-imports --disallow-untyped-defs --check-untyped-defs
+
+lint-strict:
+	uv run flake8 src
+	uv run mypy src --strict
+	
+.PHONY: install run debug clean lint lint-strict
---
+++ src/cli.py
@@ -0,0 +1,39 @@
+import fire  # pyright: ignore[reportMissingTypeStubs]
+
+
+class RAGCLI:
+    """Comand Line interface for the RAG system."""
+
+    def index(self, max_chunk_size: int = 2000):
+        """Builds indexing from the raw repository."""
+        print(f"Beggining indexing with max_chunk_size:{max_chunk_size}...")
+
+    def search(self, query: str, k: int):
+        """Returns top-k sources in a single query."""
+        print(f"Searching for: '{query}' (top {k})...")
+
+    def search_dataset(self, dataset_path: str, k: int, save_directory: str):
+        """Runs research for a complete dataset and stores it in a JSON."""
+        print(f"Processing dataset: {dataset_path}...")
+
+    def answer(self, query: str, k: int):
+        """Generates an answer to a single query."""
+        print(f"Generating answer to: '{query}'...")
+
+    def answer_dataset(
+            self, student_search_results_path: str, save_directory: str):
+        """Generates answers for all queries in a researched dataset."""
+        print(f"Generating answers for dataset: {student_search_results_path}")
+
+    def evaluate(
+            self,
+            student_search_results_path: str,
+            dataset_path: str,
+            k: int = 10,
+            max_context_length: int = 2000):
+        """Evaluates research quality locally (recall@k)"""
+        print("Calculating evaluation metrics...")
+
+
+def main():
+    fire.Fire(RAGCLI)  # pyright: ignore[reportUnknownMemberType]
